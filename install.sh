4b5274fadc9a9aa90784a10e30695e7d7b04b480
Chris <electronicsleep@gmail.com>
2018-11-25 20:29:20 -0800

    dep

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt-get install libsdl2-2.0-0 -y
+apt-get install libsdl2-dev -y
+apt-get install libsdl2-ttf-dev -y
+apt-get install libsdl2-image-dev -y
+apt-get install libsdl2-mixer-dev -y
+apt-get install libsdl2-ttf-dev -y
+apt-get install freeglut3-dev -y
+apt-get install libglew-dev -y
