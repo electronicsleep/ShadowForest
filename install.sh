9262aec8bcab68c1f20e3e24bc810f8516e639bf
Chris <electronicsleep@gmail.com>
2020-07-12 23:14:29 -0700

    update-install

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
+apt-get install make g++ -y
 apt-get install libsdl2-2.0-0 -y
 apt-get install libsdl2-dev -y
 apt-get install libsdl2-ttf-dev -y
 apt-get install libsdl2-image-dev -y
 apt-get install libsdl2-mixer-dev -y
-apt-get install libsdl2-ttf-dev -y
 apt-get install freeglut3-dev -y
 apt-get install libglew-dev -y
 apt-get install libsdl2-ttf-dev -y
