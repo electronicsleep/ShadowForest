17a7c019d78a2fe2f4a71884788a765c76a126cd
Chris <electronicsleep@gmail.com>
2022-09-22 08:35:55 -0700

    update-setup-build

--- cicd/build.sh
+++ cicd/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 cd src
 make
 ls -lh
--- setup.sh
+++ setup.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-set -e
-apt-get install make g++ -y
-apt-get install libsdl2-2.0-0 -y
-apt-get install libsdl2-dev -y
-apt-get install libsdl2-ttf-dev -y
-apt-get install libsdl2-image-dev -y
-apt-get install libsdl2-mixer-dev -y
-apt-get install freeglut3-dev -y
-apt-get install libglew-dev -y
+set -ex
+apt-get install -y make g++ libsdl2-2.0-0 libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-mixer-dev freeglut3-dev libglew-dev
